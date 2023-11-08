1fb82552e160868151a52e94e75a6ba6fd03e101
Mohand Emad Attya <102548631+mohandemadx@users.noreply.github.com>
2023-11-08 19:02:20 +0200

    second commit

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ from os import path
 from PyQt5.uic import loadUiType
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QFileDialog, QListWidgetItem, QVBoxLayout, QWidget
 
+
 FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "design.ui"))
 
 class MainApp(QMainWindow, FORM_CLASS):
     def __init__(self, parent=None):
         super(MainApp, self).__init__(parent)
         QMainWindow.__init__(self)
+
         self.setupUi(self)
+        self.setWindowTitle("Signal Equalizer")
 
         
 def main():
     app = QApplication(sys.argv)
+    
     with open("style.qss", "r") as f:
         _style = f.read()
         app.setStyleSheet(_style)
-        
+
     window = MainApp()
     window.show()
     app.exec_()
